2766ea28e47e5cf278dcc9d39304cb6bcdf286c9
maxwell-arzadon <arzadonmaxwelljohn@gmail.com>
2024-09-21 04:10:01 +0800

    Update scripts

--- public/scripts/glide.js
+++ public/scripts/glide.js
@@ -1,12 +1,12 @@
-import Glide from '../node_modules/@glidejs/glide/dist/glide.esm.js';
+import Glide from '@glidejs/glide';
 
 const glide = new Glide('.glide', {
     type: 'carousel',
     perView: 1,
     gap: 20,
     breakpoints: {
-    1024: { perView: 1 },
-    600: { perView: 1 }
+        1024: { perView: 1 },
+        600: { perView: 1 }
     }
 });
 
