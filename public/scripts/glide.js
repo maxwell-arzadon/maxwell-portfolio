d05d69b9969ee1d5ffb08914ffb93103975a8e29
maxwell-arzadon <arzadonmaxwelljohn@gmail.com>
2024-09-21 03:53:04 +0800

    Update path

--- public/scripts/glide.js
+++ public/scripts/glide.js
@@ -1,4 +1,4 @@
-import Glide from '../../node_modules/@glidejs/glide/dist/glide.esm.js';
+import Glide from '../node_modules/@glidejs/glide/dist/glide.esm.js';
 
 const glide = new Glide('.glide', {
     type: 'carousel',
