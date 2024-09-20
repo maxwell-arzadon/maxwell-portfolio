84196586be939de9f44690597aea5d387af9bdde
maxwell-arzadon <arzadonmaxwelljohn@gmail.com>
2024-09-21 04:04:48 +0800

    Update glide

--- public/scripts/scrollreveal.js
+++ public/scripts/scrollreveal.js
@@ -1,15 +1,15 @@
-import ScrollReveal from 'scrollreveal';
+// import ScrollReveal from '/scrollreveal';
 
-const sr = ScrollReveal({
-  origin: 'bottom', 
-  distance: '50px',
-  duration: 1000,
-  delay: 200,
-  easing: 'ease-in-out',
-  reset: true, // Set to true to re-trigger animations on scroll
-});
+// const sr = ScrollReveal({
+//   origin: 'bottom', 
+//   distance: '50px',
+//   duration: 1000,
+//   delay: 200,
+//   easing: 'ease-in-out',
+//   reset: true, // Set to true to re-trigger animations on scroll
+// });
 
-// Reveal elements with the specified selector
-sr.reveal('.reveal', {
-  interval: 200, // Delay between each revealed element
-});
+// // Reveal elements with the specified selector
+// sr.reveal('.reveal', {
+//   interval: 200, // Delay between each revealed element
+// });
