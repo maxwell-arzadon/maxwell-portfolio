eb17fe54bc12c7a8b09c591ffa1b9e5bfb470ba9
maxwell-arzadon <arzadonmaxwelljohn@gmail.com>
2024-09-21 03:46:38 +0800

    Update javascript

---
+++ public/scripts/glide.js
@@ -0,0 +1,13 @@
+import Glide from '../../node_modules/@glidejs/glide/dist/glide.esm.js';
+
+const glide = new Glide('.glide', {
+    type: 'carousel',
+    perView: 1,
+    gap: 20,
+    breakpoints: {
+    1024: { perView: 1 },
+    600: { perView: 1 }
+    }
+});
+
+glide.mount();
