a957463083ba4e318b3b88762227673161fff098
maxwell-arzadon <arzadonmaxwelljohn@gmail.com>
2024-09-21 12:38:18 +0800

    Move script in src

--- src/scripts/glide.js
+++ src/scripts/glide.js
@@ -1,4 +1,4 @@
-import Glide from '../node_modules/@glidejs/glide/dist/glide.esm.js';
+import Glide from '../../node_modules/@glidejs/glide/dist/glide.esm.js';
 console.log("Initializing Glide...");
 const glide = new Glide('.glide', {
     type: 'carousel',
